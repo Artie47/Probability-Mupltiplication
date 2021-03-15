61dbf655869e262ca05f525fb6e4a2bc7b873805
Artie47 <45467552+Artie47@users.noreply.github.com>
2021-03-16 00:01:34 +0300

    Add files via upload

---
+++ Probability Multiplication/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(230, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сразу 100 итераций!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "N: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество наборов ББ: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Количество наборов БЧ: 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 330);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Количество наборов ЧЧ: 0";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Silver;
+            chartArea1.AlignmentOrientation = System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(458, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Черный-Черный";
+            series1.MarkerColor = System.Drawing.Color.Black;
+            series1.MarkerSize = 20;
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Черный-Черный";
+            series1.XValueMember = "Черный-Черный";
+            series1.YValueMembers = "bb";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Белый-Черный";
+            series2.MarkerBorderColor = System.Drawing.Color.Gray;
+            series2.MarkerColor = System.Drawing.Color.Gray;
+            series2.MarkerSize = 20;
+            series2.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series2.Name = "Белый-Черный";
+            series2.XValueMember = "Белый-Черный";
+            series2.YValueMembers = "wb";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Белый-Белый";
+            series3.MarkerBorderColor = System.Drawing.Color.White;
+            series3.MarkerColor = System.Drawing.Color.White;
+            series3.MarkerSize = 20;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Белый-Белый";
+            series3.XValueMember = "Белый-Белый";
+            series3.YValueMembers = "ww";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(340, 426);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.Name = "Title1";
+            title1.Text = "График распределения комбинаций";
+            this.chart1.Titles.Add(title1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(23, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(260, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Относительная частота P(ББ) = ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 45);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Начать!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.Silver;
+            chartArea2.BackColor = System.Drawing.Color.Silver;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(782, 13);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.MarkerImageTransparentColor = System.Drawing.Color.Maroon;
+            series4.Name = "P(ББ)";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(358, 425);
+            this.chart2.TabIndex = 10;
+            this.chart2.Text = "chart2";
+            title2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title2.Name = "Title1";
+            title2.Text = "Зависимость значения относительной частоты   P(ББ) от количества испытаний";
+            this.chart2.Titles.Add(title2);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(196, 258);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(240, 45);
+            this.trackBar2.TabIndex = 12;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(1144, 445);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Вытаскивание шариков";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Probability Multiplication/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp3
+{
+   
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        int k = 0;
+
+        int bb = 0;
+        int wb = 0;
+        int ww = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw()
+        {
+
+            Bitmap bmp1 = new Bitmap(200, 200);
+            Graphics graph1 = Graphics.FromImage(bmp1);
+            Brush pen1 = new LinearGradientBrush(new Point(0,10), new Point(0,160), Color.White, Color.LightGray);
+            graph1.FillEllipse(pen1, 10, 10, 150, 150);
+
+
+            Bitmap bmp2 = new Bitmap(200, 200);
+            Graphics graph2 = Graphics.FromImage(bmp2);
+            Brush pen2 = new LinearGradientBrush(new Point(0, 10), new Point(0, 250), Color.Black, Color.DarkGray);
+            graph2.FillEllipse(pen2, 10, 10, 150, 150);
+
+
+            char[] Basket1 = { 'Ч', 'Ч', 'Ч', 'Ч', 'Б', 'Б', 'Б', 'Б', 'Б' };
+            char[] Basket2 = { 'Ч', 'Ч', 'Ч', 'Ч', 'Б', 'Б', 'Б', 'Б', 'Б' };
+
+            
+
+            char pulled_1 = Basket1[rnd.Next(Basket1.Length)];
+            char pulled_2 = Basket2[rnd.Next(Basket2.Length)];
+
+
+            if (pulled_1 == 'Ч' && pulled_2 == 'Ч')
+            {
+                pictureBox1.Image = bmp2;
+                pictureBox2.Image = bmp2;
+
+                bb++;
+            }
+            else if (pulled_1 == 'Ч' && pulled_2 == 'Б')
+            {
+                pictureBox1.Image = bmp2;
+                pictureBox2.Image = bmp1;
+
+                wb++;
+
+            }
+            else if (pulled_1 == 'Б' && pulled_2 == 'Ч')
+            {
+                pictureBox1.Image = bmp1;
+                pictureBox2.Image = bmp2;
+
+                wb++;
+            }
+            else if(pulled_1 == 'Б' && pulled_2 == 'Б')
+            {
+                pictureBox1.Image = bmp1;
+                pictureBox2.Image = bmp1;
+
+                ww++;
+            }
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int a = 0; a < 100; a++)
+            {
+                Draw();
+                k++;
+                button1.Text = "Повторить +100";
+                label1.Text = "N: " + Convert.ToString(k);
+                label2.Text = "Количество наборов ББ: " + Convert.ToString(ww);
+                label3.Text = "Количество наборов БЧ: " + Convert.ToString(wb);
+                label4.Text = "Количество наборов ЧЧ: " + Convert.ToString(bb);
+                label5.Text = "Относительная частота P(ББ) = " + Convert.ToString(Math.Round((double)ww / (double)k, 6));
+
+
+                chart1.Series["Черный-Черный"].Points.AddXY(1, Convert.ToString(bb));
+                chart1.Series["Белый-Черный"].Points.AddXY(2, Convert.ToString(wb));
+                chart1.Series["Белый-Белый"].Points.AddXY(3, Convert.ToString(ww));
+
+
+
+                chart2.Series["P(ББ)"].Points.AddXY(k, (double)ww / (double)k);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Draw();
+            k++;
+            button2.Text = "Повторить +1";
+            label1.Text = "N: " + Convert.ToString(k);
+            label2.Text = "Количество наборов ББ: " + Convert.ToString(ww);
+            label3.Text = "Количество наборов БЧ: " + Convert.ToString(wb);
+            label4.Text = "Количество наборов ЧЧ: " + Convert.ToString(bb);
+            label5.Text = "Относительная частота P(ББ) = " + Convert.ToString(Math.Round((double)ww / k, 6));
+
+
+            chart1.Series["Черный-Черный"].Points.AddXY(1, Convert.ToString(bb));
+            chart1.Series["Белый-Черный"].Points.AddXY(2, Convert.ToString(wb));
+            chart1.Series["Белый-Белый"].Points.AddXY(3, Convert.ToString(ww));
+
+
+            chart2.Series["P(ББ)"].Points.AddXY(k, (double)ww / k);
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            trackBar2.Maximum = 5000;
+            
+
+            for (int a = 0; a < trackBar2.Value; a++)
+            {
+                k++;
+                Draw();
+                
+                label1.Text = "N: " + Convert.ToString(k);
+                label2.Text = "Количество наборов ББ: " + Convert.ToString(ww);
+                label3.Text = "Количество наборов БЧ: " + Convert.ToString(wb);
+                label4.Text = "Количество наборов ЧЧ: " + Convert.ToString(bb);
+                label5.Text = "Относительная частота P(ББ) = " + Convert.ToString(Math.Round((double)ww / (double)k, 6));
+
+                chart1.Series["Черный-Черный"].Points.AddXY(1, Convert.ToString(bb));
+                chart1.Series["Белый-Черный"].Points.AddXY(2, Convert.ToString(wb));
+                chart1.Series["Белый-Белый"].Points.AddXY(3, Convert.ToString(ww));
+
+                chart2.Series["P(ББ)"].Points.AddXY(k+trackBar2.Value, (double)ww / (double)k);
+
+            }
+        
+            
+
+            
+
+            
+
+        }
+    }
+}
